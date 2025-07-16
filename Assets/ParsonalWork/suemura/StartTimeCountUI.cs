60c86c62f9e6faeedfcb7e64d21c4d2b4a1c660d
末村優輝 <tca2304j70012@edu.tca.ac.jp>
2025-07-16 14:11:10 +0900

    追加したところ ・フォルダ イメージ画像フォルダを追加し、画像を入れました ・スクリプトの追加 ゲーム開始時にUIのイメージを変更する処理 ・シーンの変更 オブジェクトを削除しました。

---
+++ Assets/ParsonalWork/suemura/StartTimeCountUI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class StartTimeCountUI : MonoBehaviour
+{
+    /// <summary>
+    /// イメージを表示させる場所
+    /// </summary>
+    [SerializeField] private Image displayImage;
+    /// <summary>
+    /// ゲーム開始時のイメージ配列
+    /// </summary>
+    [SerializeField] private Sprite[] GameStartImg;
+    /// <summary>
+    /// 表示間隔
+    /// </summary>
+    [SerializeField] private float interval = 1f;
+
+
+    private void Start()
+    {
+        StartCoroutine(CountDownImage());
+    }
+
+    private IEnumerator CountDownImage()
+    {
+       for(int i = 0; i < GameStartImg.Length; i++)
+       {
+            displayImage.sprite = GameStartImg[i];
+            displayImage.gameObject.SetActive(true);
+            yield return new WaitForSeconds(interval);
+       }
+
+       displayImage.gameObject.SetActive(false);
+    }
+}
